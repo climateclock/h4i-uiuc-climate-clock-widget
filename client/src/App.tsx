8ce92b9f1dbf13ef15bbc7bf0c65bb4eacaddc51
daniel-moon32 <dm32@illinois.edu>
2022-02-15 20:15:17 -0600

    add api wrapper and exmaples

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Example } from '../src/components/example/Example'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header"></header>
+      <Example exampleProp="test"></Example>
     </div>
   )
 }
--- client/src/components/example/Example.tsx
+++ client/src/components/example/Example.tsx
@@ -1,17 +1,32 @@
-import React, { FC, ReactElement } from 'react'
+import React, { FC, ReactElement, useState } from 'react'
 import styled from 'styled-components'
+import { useEffect } from 'react'
+import { get } from '../../api/config'
+import { ApiRequestUrl } from '../../api/constants'
 
 interface ExampleProps {
   exampleProp: string
 }
 
+interface ApiOutput {
+  status: string
+}
+
 const StyledDiv = styled.div`
-  background: $(({theme}) => theme.background);
+  background: ${({ theme }) => theme.background};
 `
 
 // Comment describing component
-export const Example: FC<ExampleProps> = ({ exampleProp }): ReactElement => (
-  <StyledDiv className="example-wrapper">
-    <h1 id="component-header">{exampleProp}</h1>
-  </StyledDiv>
-)
+export const Example: FC<ExampleProps> = ({ exampleProp }): ReactElement => {
+  const [exampleData, setExampleData] = useState<any | null>(null)
+  async function exampleFunc() {
+    get('/', 'clock').then((out) => setExampleData(out))
+  }
+  const exampleApi = exampleFunc()
+  return (
+    <StyledDiv className="example-wrapper">
+      <h1 id="component-header">{exampleProp}</h1>
+      {exampleData && exampleData.status}
+    </StyledDiv>
+  )
+}
