7edad223bc697559b35e62f3154bc01558b94ce0
Daniel Moon <36148173+daniel-moon32@users.noreply.github.com>
2022-02-19 17:31:09 -0600

    Edit global theme constants (#11)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Example } from '../src/components/example/Example'
 import { ThemeProvider } from 'styled-components'
 import GlobalStyle, { theme } from './components/ui/GlobalStyle'
+import { WindowSize } from '@reach/window-size'
 
 function App() {
   return (
@@ -10,6 +11,9 @@ function App() {
         <header className="App-header"></header>
         <Example exampleProp="test"></Example>
       </div>
+      <WindowSize>
+        {(windowSize) => <GlobalStyle windowSize={windowSize} />}
+      </WindowSize>
     </ThemeProvider>
   )
 }
