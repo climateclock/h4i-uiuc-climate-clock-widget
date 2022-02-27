24ed9fa04f156d7f61660ea3d3024da96ff7a530
sahilkamboj3 <kambojsahil327@gmail.com>
2022-02-27 02:18:13 -0600

    added comment

--- client/src/components/Lifeline.tsx
+++ client/src/components/Lifeline.tsx
@@ -4,7 +4,7 @@ import { useState, useEffect } from 'react'
 
 function Lifeline(props: LifelinePropsInterface) {
   const seconds = 1 // running every every seconds * 1000
-  const decimalPlaces = !props.resolution ? 0 : Math.log10(1 / props.resolution) // set the precision of value
+  const decimalPlaces = !props.resolution ? 0 : Math.log10(1 / props.resolution) // set the precision of value (ie. props.resolution = 1e-9 => 9)
   const rate = !props.rate ? 0 : props.rate // store rate at which to update value
   const isMoneyVal = !props.unit || props.unit.charAt(0) !== '$' ? false : true // used to fix monetary units passed in (ie. $)
   const [value, setValue] = useState<number>(
