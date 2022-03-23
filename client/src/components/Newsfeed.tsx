c34b4e1a14762bd03bf8ea1a5c7e3ea30fa42e3e
eeshabarua <eesha.barua@gmail.com>
2022-03-22 20:12:34 -0500

    Clean Up Code

--- client/src/components/Newsfeed.tsx
+++ client/src/components/Newsfeed.tsx
@@ -4,16 +4,6 @@ import { HeadlinesInterface } from '../interfaces'
 import { MarqueeContainer } from './styles'
 
 const Newsfeed = (props: HeadlinesInterface) => {
-  // let concatenatedHeadlines = ''
-  // props.news.forEach(
-  //   (article) => (concatenatedHeadlines += article.headline?.toUpperCase),
-  // )
-  // for (let i = 0; i < props.headlines.length; i++) {
-  //   concatenatedHeadlines = concatenatedHeadlines.toUpperCase()
-  //   console.log(i)
-  // }
-  // return <Ticker>{() => <Headlines headlines={props.headlines} />}</Ticker>
-  // return <Ticker>{({ index }) => <p>{props.headlines[index]}</p>}</Ticker>
   return (
     <MarqueeContainer>
       <Marquee gradient={false} speed={200}>
