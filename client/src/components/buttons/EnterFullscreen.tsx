d332a8b8b13e64d7ffbe2f1ac5d8b5cee2c605f4
Aaron Alexander <a.alexander72003@gmail.com>
2022-04-01 11:05:44 -0500

    Change app and fullscreen components

--- client/src/components/buttons/EnterFullscreen.tsx
+++ client/src/components/buttons/EnterFullscreen.tsx
@@ -1,8 +1,4 @@
-import styled from 'styled-components'
-import { useState, useEffect } from 'react'
-import { FullScreen, useFullScreenHandle } from 'react-full-screen'
 import { FullscreenEnter } from '@styled-icons/open-iconic'
-import { useLinkClickHandler } from 'react-router-dom'
 
 function EnterFullscreen({ handle }: { handle: any }) {
   return <FullscreenEnter size="2%" onClick={handle} />
--- client/src/components/buttons/ExitFullscreen.tsx
+++ client/src/components/buttons/ExitFullscreen.tsx
@@ -1,8 +1,5 @@
 import styled from 'styled-components'
-import { useState, useEffect } from 'react'
-import { useFullScreenHandle } from 'react-full-screen'
 import { FullscreenExit } from '@styled-icons/open-iconic'
-import { AnySoaRecord } from 'dns'
 const ExitButton = styled.div`
   position: absolute;
   top: 50px;
