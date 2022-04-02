89d45c54aabedcd6554804fbda936fedc923a5a9
Aaron Alexander <a.alexander72003@gmail.com>
2022-04-02 17:11:52 -0500

    Remove last of fullscreen exit styling

--- client/src/components/buttons/ExitFullscreen.tsx
+++ client/src/components/buttons/ExitFullscreen.tsx
@@ -2,8 +2,6 @@ import styled from 'styled-components'
 import { FullscreenExit } from '@styled-icons/open-iconic'
 const ExitButton = styled.div`
   color: white;
-  top: 500px;
-  width: 500px;
 `
 
 function ExitFullscreen({ handle }: { handle: any }) {
