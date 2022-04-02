50025083569d282199d6a73a53fe36127c4252ad
Aaron Alexander <82335572+AaronAlexander-debug@users.noreply.github.com>
2022-04-02 17:39:07 -0500

    Fullscreen button (#35)
    * Broken connection w/ API
    
    * Merge branch 'clock-screen' of https://github.com/hack4impact-uiuc/climate-clock into clock-screen
    
    * Fix font size
    
    * Connect w/ API
    
    * Fix some styling
    
    * Remove console statements
    
    * Cleanup file
    
    * Clean up file
    
    * Fix lint errors
    
    * Test
    
    * Optimize usage of toggle component in app
    
    * Remove unused imports
    
    * Remove imports
    
    * Implement basic enter and exit fullscreen buttons
    
    * Add basic button styling
    
    * Enable conditional rendering for fullscreen button
    
    * Add conditional fullscreen feature + UI changes
    
    * Commit small changes from previous version
    
    * Change app and fullscreen components
    
    * Remove absolute position of fullscreen exit button
    
    * Remove last of fullscreen exit styling
    
    * Include fullscreen feature for newest app
    
    Co-authored-by: Your Name <riyaj3@illinois.edu>
    Co-authored-by: daniel-moon32 <dm32@illinois.edu>

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,10 +3,9 @@ import { WindowSize } from '@reach/window-size'
 import { ThemeProvider } from 'styled-components'
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-
 import Lifeline from './components/Lifeline'
+import { FullScreen, useFullScreenHandle } from 'react-full-screen'
 import { ModuleResInterface, NewsInterface } from './interfaces'
-// import { get } from './api/config'
 import GlobalStyle, { theme } from './components/ui/GlobalStyle'
 import Newsfeed from './components/Newsfeed'
 import {
@@ -18,6 +17,8 @@ import {
 import Clock from './components/clock/Clock'
 import LanguageCustomization from './components/LanguageCustomizationForm'
 import LifelineCreation from './pages/lifelineCreation'
+import EnterFullscreen from './components/buttons/EnterFullscreen'
+import ExitFullscreen from './components/buttons/ExitFullscreen'
 import { ERROR_MSG, URL } from './util/constants'
 import { getData } from './util/util'
 
@@ -29,7 +30,8 @@ function App() {
   )
   const [newsfeedModules, setNewsfeedModules] = useState<NewsInterface[]>([])
   const [errorFlag, setErrorFlag] = useState<boolean>(false)
-
+  const handle = useFullScreenHandle()
+  const [showFullscreenButton, setFullscreenButton] = useState(false)
   /* Sets the lifeline modules upon load and every defaultLanguage change */
   useEffect(() => {
     getData(
@@ -45,45 +47,55 @@ function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/langForm" element={<LanguageCustomization />} />
-          <Route path="/moduleForm" element={<LifelineCreation />} />
-          <Route
-            path="/"
-            element={
-              <>
-                <Clock
-                  timestamp={modules && modules[0] && modules[0].timestamp}
-                />
-                {!errorFlag ? (
-                  lifelineModules.map((module) => (
-                    <Lifeline
-                      key={module['description']}
-                      title={returnFirstString(module['labels'])}
-                      module_type={toUpperCase(module['flavor'])}
-                      value={module['initial']}
-                      unit={returnFirstString(module['unit_labels'])}
-                      rate={module['rate']}
-                      resolution={module['resolution']}
-                    />
-                  ))
-                ) : (
-                  <h1>{ERROR_MSG}</h1>
-                )}
-                {!errorFlag ? (
-                  <Newsfeed headline={getHeadlines(newsfeedModules)} />
-                ) : (
-                  <h1>{ERROR_MSG}</h1>
-                )}
-              </>
-            }
-          />
-        </Routes>
-      </BrowserRouter>
-      <WindowSize>
-        {(windowSize) => <GlobalStyle windowSize={windowSize} />}
-      </WindowSize>
+      <FullScreen
+        handle={handle}
+        onChange={() => setFullscreenButton(!showFullscreenButton)}
+      >
+        <BrowserRouter>
+          <Routes>
+            <Route path="/langForm" element={<LanguageCustomization />} />
+            <Route path="/moduleForm" element={<LifelineCreation />} />
+            <Route
+              path="/"
+              element={
+                <>
+                  <Clock
+                    timestamp={modules && modules[0] && modules[0].timestamp}
+                  />
+                  {!errorFlag ? (
+                    lifelineModules.map((module) => (
+                      <Lifeline
+                        key={module['description']}
+                        title={returnFirstString(module['labels'])}
+                        module_type={toUpperCase(module['flavor'])}
+                        value={module['initial']}
+                        unit={returnFirstString(module['unit_labels'])}
+                        rate={module['rate']}
+                        resolution={module['resolution']}
+                      />
+                    ))
+                  ) : (
+                    <h1>{ERROR_MSG}</h1>
+                  )}
+                  {!errorFlag ? (
+                    <Newsfeed headline={getHeadlines(newsfeedModules)} />
+                  ) : (
+                    <h1>{ERROR_MSG}</h1>
+                  )}
+                </>
+              }
+            />
+          </Routes>
+        </BrowserRouter>
+        <WindowSize>
+          {(windowSize) => <GlobalStyle windowSize={windowSize} />}
+        </WindowSize>
+        {showFullscreenButton ? (
+          <EnterFullscreen handle={handle.enter} />
+        ) : (
+          <ExitFullscreen handle={handle.exit} />
+        )}
+      </FullScreen>
     </ThemeProvider>
   )
 }
