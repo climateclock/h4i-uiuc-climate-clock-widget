e142ff22e33af7661b0cafa21b4f9e1ba7853bd7
sahilkamboj3 <kambojsahil327@gmail.com>
2022-04-11 20:55:29 -0500

    lifeline values saved

---
+++ client/src/components/embed/LifelineEmbed.tsx
@@ -0,0 +1,117 @@
+import { LifelinePropsInterface } from '../../interfaces'
+import { useState, useEffect } from 'react'
+import styled from 'styled-components'
+import Header from '../ui/Header'
+
+const VALUE_UNIT_MARGIN: number = 1
+
+const Container = styled.div`
+  & {
+    font-family: ${({ theme }) => theme.fonts};
+    font-weight: bold;
+    background: ${({ theme }) => theme.blue};
+    height: 14.666666667vh;
+    @media only screen and (max-height: 700px) {
+      height: 35vh;
+    }
+
+    width: 100vw;
+  }
+`
+const LabelContainer = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  font-size: max(1rem, min(1.5rem, 3vw));
+  height: 3vh;
+  margin-bottom: 32px;
+
+  @media only screen and (max-height: 700px) {
+    height: 7vh;
+  }
+`
+
+const ContentContainer = styled(LabelContainer)`
+  width: 100%;
+`
+
+const Value = styled.div`
+  display: flex;
+  align-items: center;
+  font-size: 3em;
+  margin-right: ${VALUE_UNIT_MARGIN}vw;
+  margin-left: ${VALUE_UNIT_MARGIN}vw;
+  margin-top: 25px;
+`
+
+const Unit = styled.div`
+  font-size: 2em;
+  margin-left: ${VALUE_UNIT_MARGIN}vw;
+`
+
+interface LifelifeEmbedPropsInterface extends LifelinePropsInterface {
+  lifelineIndex: number
+  updateSavedValue: (index: number, value: number) => void
+}
+
+function Lifeline({
+  title,
+  module_type,
+  value,
+  resolution,
+  rate,
+  unit,
+  lifelineIndex,
+  updateSavedValue,
+}: LifelifeEmbedPropsInterface) {
+  const seconds = 0.1 // running every every seconds * 1000
+  const decimalPlaces = !resolution ? 0 : Math.log10(1 / resolution) // set the precision of value (ie. props.resolution = 1e-9 => 9)
+  const cleanedRate = !rate ? 0 : rate // store rate at which to update value
+  const isMoneyVal = !unit || unit.charAt(0) !== '$' ? false : true // used to fix monetary units passed in (ie. $)
+  const [llVal, setLLVal] = useState<number>(
+    !value ? cleanedRate : value + cleanedRate,
+  )
+
+  /* update lifeline value within interval */
+  useEffect(() => {
+    let interval = setInterval(() => {
+      if (rate !== 0) {
+        setLLVal((llVal) => llVal + cleanedRate)
+      }
+    }, seconds * 1000)
+
+    return () => {
+      clearInterval(interval)
+    }
+  })
+
+  useEffect(() => {
+    return () => {
+      console.log(
+        'calling update on ' +
+          lifelineIndex.toString() +
+          ' ' +
+          llVal.toString(),
+      )
+      updateSavedValue(lifelineIndex, llVal)
+    }
+  }, [llVal]) // check this
+
+  return (
+    <Container>
+      <Header
+        moduleType={module_type}
+        title={title}
+        themeColor={({ theme }) => theme.blue}
+      />
+      <ContentContainer>
+        <Value>
+          {isMoneyVal && '$'}
+          {llVal.toFixed(decimalPlaces)}
+        </Value>
+        <Unit> {!isMoneyVal ? unit : unit.substring(1)}</Unit>
+      </ContentContainer>
+    </Container>
+  )
+}
+
+export default Lifeline
--- client/src/components/embed/LifelinesEmbed.tsx
+++ client/src/components/embed/LifelinesEmbed.tsx
@@ -7,7 +7,8 @@
  *
  */
 
-import Lifeline from '../lifelines/Lifeline'
+// import Lifeline from '../lifelines/Lifeline'
+import Lifeline from './LifelineEmbed'
 import { ModuleResInterface } from '../../interfaces'
 import { returnFirstString, toUpperCase } from '../../utils/utils'
 import useWindowDimensions from '../../hooks/useWindowdimensions'
@@ -26,16 +27,9 @@ export default function LifelinesEmbed({
   let lifelineDisplayNum = dimensions.height > 700 ? displayNum : 1
   const [lifelineIndex, setLifelineIndex] = useState<number>(0)
   const LIFELINE_DURATION = 5 // seconds displayed per lifeline
-  //   const [lifelineSavedValues, setLifelineSavedValues] = useState<
-  //     number[] | undefined[]
-  //   >(Array(lifelineDisplayNum))
-
-  //   useEffect(() => {
-  //     for (let i = 0; i < displayNum; i++) {
-  //         lifelineSavedValues[i] = lifeLineData[i]['initial']
-  //     }
-  //     setLifelineIndex([])
-  //   },[])
+  const [lifelineSavedValues, setLifelineSavedValues] = useState<
+    (number | undefined)[]
+  >(Array(lifelineDisplayNum)) // saved lifeline values after set time duration
 
   useEffect(() => {
     let interval = setInterval(() => {
@@ -49,15 +43,32 @@ export default function LifelinesEmbed({
     }
   }, [])
 
+  useEffect(() => {
+    for (let i = 0; i < lifelineDisplayNum; i++) {
+      if (lifeLineData[i]) lifelineSavedValues[i] = lifeLineData[i]['initial']
+    }
+    setLifelineSavedValues([...lifelineSavedValues])
+  }, [lifeLineData])
+
+  /* used to update saved values to continue for next time duration */
+  const updateSavedValue = (index: number, value: number) => {
+    let newLifelineSavedValues = lifelineSavedValues
+    newLifelineSavedValues[index] = value
+    setLifelineSavedValues(newLifelineSavedValues)
+  }
+
   return (
     <>
       {lifeLineData.slice(lifelineIndex, lifelineIndex + 1).map((module) => {
         return (
           <Lifeline
-            key={module['description']}
+            key={lifelineIndex.toString() + module['description']}
+            updateSavedValue={updateSavedValue}
+            lifelineIndex={lifelineIndex}
             title={returnFirstString(module['labels'])}
             module_type={toUpperCase(module['flavor'])}
-            value={module['initial']}
+            // value={module['initial']}
+            value={lifelineSavedValues[lifelineIndex]}
             unit={returnFirstString(module['unit_labels'])}
             rate={module['rate']}
             resolution={module['resolution']}
