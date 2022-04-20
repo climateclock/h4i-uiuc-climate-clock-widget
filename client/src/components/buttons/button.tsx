26278689d3d87def8397ba1f238de0b9feeb38d4
Nikhil Gargeya <nikhil27gargeya@gmail.com>
2022-04-19 19:44:54 -0500

    styled button added new theme colors

--- client/src/components/buttons/button.tsx
+++ client/src/components/buttons/button.tsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components'
 
 const StyledButton = styled.button`
-    background-color: #1D5479;
+    background-color: ${({ theme }) => theme.buttonBackground };
     border: none;
     cursor: pointer;
     text-align: center;
@@ -13,26 +13,26 @@ const StyledButton = styled.button`
     font-size: 12px;
     font-weight: 700;
     padding: 10px 15px;
-//    font-family: <Lato></Lato>;
+    font-family:  ${({ theme }) => theme.secondaryFonts };
     &:hover {
         background-image: linear-gradient(rgba(0, 0, 0, 0.06) 0 0);
     }
 `;
 
 const HideButton = styled(StyledButton)`
-    background: white;
+    background: ${({ theme }) => theme.background };
     border: 1px solid gray;
     cursor: pointer;
     text-align: center;
     text-decoration: none;
     margin: 4px 2px;
     display: inline-block;
-    color: black;
+    color: ${({ theme }) => theme.text };
     border-radius: 3px;
     font-size: 12px;
     padding: 6.5px 20px;
     font-weight: 500;
-    font-family: "Lato"
+    font-family:  ${({ theme }) => theme.secondaryFonts };
     &:hover {
        background-color: #F1F1F1;
     }
