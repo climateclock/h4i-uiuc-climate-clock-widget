e9e6c4d01dda4b7f0fa3c0cb93bc6a7caff8c4e9
daniel-moon32 <dm32@illinois.edu>
2022-04-19 19:06:27 -0500

    remove header bug

--- client/src/components/ui/Header.tsx
+++ client/src/components/ui/Header.tsx
@@ -65,7 +65,7 @@ export default function Header({ moduleType, title, themeColor }: HeaderProps) {
       <HeaderBorder />
       <LabelContainer>
         <Module>{moduleType}</Module>
-        <Title color={themeColor}>{title}</Title>
+        <Title>{title}</Title>
       </LabelContainer>
     </>
   )
