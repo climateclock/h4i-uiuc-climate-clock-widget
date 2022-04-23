5f11d931b65824cd7103485bff0d54d6cd1d0f7d
eeshabarua <eesha.barua@gmail.com>
2022-04-23 16:31:34 -0500

    Add Styling

--- client/src/pages/settings.tsx
+++ client/src/pages/settings.tsx
@@ -1,7 +1,29 @@
 import LanguageCustomization from '../components/settings/LanguageCustomizationForm'
+import styled from 'styled-components'
+
+const SettingsTitle = styled.div`
+  font-family: ${({ theme }) => theme.fonts};
+  font-size: 1.625rem;
+  margin: 4em 0em 0em 4em;
+  font-color: ${({ theme }) => theme.blue};
+`
+
+const HeadingTitle = styled.div`
+  font-family: ${({ theme }) => theme.fonts};
+  font-size: 1.25rem;
+  margin: 3em 0em 0em 4em;
+  font-color: ${({ theme }) => theme.blue};
+`
 
 function Settings() {
-  return <LanguageCustomization />
+  return (
+    <>
+      <SettingsTitle>Settings</SettingsTitle>
+      <LanguageCustomization />
+      <HeadingTitle>News Ticker</HeadingTitle>
+      <p> Turn off/on the bottom news on your clock, </p>
+    </>
+  )
 }
 
 export default Settings
