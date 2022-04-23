88f20de35d71fa12e36855ec6c3736625ccd317c
Nikhil Gargeya <nikhil27gargeya@gmail.com>
2022-04-23 17:11:43 -0500

    ran yarn lint on button file

--- client/src/components/buttons/button.tsx
+++ client/src/components/buttons/button.tsx
@@ -19,24 +19,24 @@ const StyledButton = styled.button`
     }
 `;
 
-const HideButton = styled(StyledButton)`
-    background: ${({ theme }) => theme.background };
-    border: 1px solid gray;
-    cursor: pointer;
-    text-align: center;
-    text-decoration: none;
-    margin: 4px 2px;
-    display: inline-block;
-    color: ${({ theme }) => theme.text };
-    border-radius: 3px;
-    font-size: 12px;
-    padding: 6.5px 20px;
-    font-weight: 500;
-    font-family:  ${({ theme }) => theme.secondaryFonts };
-    &:hover {
-       background-color: #F1F1F1;
-    }
-`;
+// const HideButton = styled(StyledButton)`
+//     background: ${({ theme }) => theme.background };
+//     border: 1px solid gray;
+//     cursor: pointer;
+//     text-align: center;
+//     text-decoration: none;
+//     margin: 4px 2px;
+//     display: inline-block;
+//     color: ${({ theme }) => theme.text };
+//     border-radius: 3px;
+//     font-size: 12px;
+//     padding: 6.5px 20px;
+//     font-weight: 500;
+//     font-family:  ${({ theme }) => theme.secondaryFonts };
+//     &:hover {
+//        background-color: #F1F1F1;
+//     }
+// `;
 
 const Button = ({ buttonLabel }) => {
     return <StyledButton>{ buttonLabel }</StyledButton>
