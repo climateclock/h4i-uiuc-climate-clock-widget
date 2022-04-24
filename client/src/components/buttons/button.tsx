a944cd7fd2c0d1c68d33446c5f51ed895ba398e4
daniel-moon32 <dm32@illinois.edu>
2022-04-24 18:07:50 -0500

    ran formatter

--- client/src/components/buttons/button.tsx
+++ client/src/components/buttons/button.tsx
@@ -1,23 +1,23 @@
 import styled from 'styled-components'
 
 const StyledButton = styled.button`
-    background-color: ${({ theme }) => theme.buttonBackground };
-    border: none;
-    cursor: pointer;
-    text-align: center;
-    text-decoration: none;
-    margin: 4px 2px;
-    display: inline-block;
-    color: white;
-    border-radius: 5px;
-    font-size: 12px;
-    font-weight: 700;
-    padding: 10px 15px;
-    font-family:  ${({ theme }) => theme.secondaryFonts };
-    &:hover {
-        background-image: linear-gradient(rgba(0, 0, 0, 0.06) 0 0);
-    }
-`;
+  background-color: ${({ theme }) => theme.buttonBackground};
+  border: none;
+  cursor: pointer;
+  text-align: center;
+  text-decoration: none;
+  margin: 4px 2px;
+  display: inline-block;
+  color: white;
+  border-radius: 5px;
+  font-size: 12px;
+  font-weight: 700;
+  padding: 10px 15px;
+  font-family: ${({ theme }) => theme.secondaryFonts};
+  &:hover {
+    background-image: linear-gradient(rgba(0, 0, 0, 0.06) 0 0);
+  }
+`
 
 // const HideButton = styled(StyledButton)`
 //     background: ${({ theme }) => theme.background };
@@ -39,6 +39,6 @@ const StyledButton = styled.button`
 // `;
 
 const Button = ({ buttonLabel }) => {
-    return <StyledButton>{ buttonLabel }</StyledButton>
+  return <StyledButton>{buttonLabel}</StyledButton>
 }
-export default Button;
+export default Button
