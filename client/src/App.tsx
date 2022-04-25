061ff82fd6dad0319165fec049b9642d8bc91f97
sahilkamboj3 <kambojsahil327@gmail.com>
2022-04-25 15:20:15 -0500

    fullscreen in App.tsx removed, comments removed

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,28 +6,19 @@ import Settings from './pages/Settings'
 import LifelineCreation from './pages/lifelineCreation'
 import Home from './pages/Home'
 import PageEmbed from './components/embed/PageEmbed'
-import { FullScreen, useFullScreenHandle } from 'react-full-screen'
-import { useState } from 'react'
 
 function App() {
-  const [showFullscreenButton, setFullscreenButton] = useState(false)
   /* Sets the lifeline modules upon load and every defaultLanguage change */
-  const handle = useFullScreenHandle()
   return (
     <ThemeProvider theme={theme}>
-      <FullScreen
-        handle={handle}
-        onChange={() => setFullscreenButton(!showFullscreenButton)}
-      >
-        <BrowserRouter>
-          <Routes>
-            <Route path="/settings" element={<Settings />} />
-            <Route path="/lifelines" element={<LifelineCreation />} />
-            <Route path="/" element={<Home />} />
-            <Route path="/embed" element={<PageEmbed />} />
-          </Routes>
-        </BrowserRouter>
-      </FullScreen>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/settings" element={<Settings />} />
+          <Route path="/lifelines" element={<LifelineCreation />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/embed" element={<PageEmbed />} />
+        </Routes>
+      </BrowserRouter>
       <WindowSize>
         {(windowSize) => <GlobalStyle windowSize={windowSize} />}
       </WindowSize>
