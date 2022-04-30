45e41095108082536b8fcb2b88a2541db4fa0259
eeshabarua <eesha.barua@gmail.com>
2022-04-29 21:05:06 -0500

    Update Styling

--- client/src/pages/Settings.tsx
+++ client/src/pages/Settings.tsx
@@ -1,6 +1,7 @@
 import { useFullScreenHandle } from 'react-full-screen'
 import styled from 'styled-components'
 
+import Toggle from '../components/buttons/Toggle'
 import CopyButton from '../components/ui/CopyButton'
 import NavBar from '../components/ui/NavBar'
 import { StyledSelect } from '../components/ui/Select'
@@ -10,53 +11,48 @@ import instagram from '../utils/icons/instagram.png'
 import twitter from '../utils/icons/twitter.png'
 import whatsapp from '../utils/icons/whatsapp.png'
 
-const SettingsSection = styled.div`
-  h1 {
-    color: ${({ theme }) => theme.headerText};
-    font-size: 30px;
-    font-family: ${({ theme }) => theme.secondaryFonts};
-    font-weight: 600;
-    font-size: 30px;
-    line-height: 36px;
-    display: flex;
-    align-items: center;
-    position: relative;
-    top: 5%;
-    bottom: 80.47%;
-    left: 5%;
-  }
-  h3 {
-    color: ${({ theme }) => theme.text};
-    font-family: ${({ theme }) => theme.secondaryFonts};
-    position: relative;
-    top: 27.89%;
-    bottom: 69.44%;
-    left: 5%;
-    font-weight: bold;
-    font-size: 20px;
-    line-height: 24px;
-  }
-  h4 {
-    color: ${({ theme }) => theme.text};
-    font-family: ${({ theme }) => theme.secondaryFonts};
-    position: relative;
-    top: 27.89%;
-    bottom: 69.44%;
-    left: 5%;
-    font-weight: 350;
-    font-size: 1rem;
-    line-height: 24px;
-  }
-  p {
-    color: ${({ theme }) => theme.text};
-    font-family: ${({ theme }) => theme.secondaryFonts};
-    position: relative;
-    top: 27.89%;
-    bottom: 69.44%;
-    left: 5%;
-    font-size: 0.75rem;
-    line-height: 24px;
-  }
+const SettingsHeading = styled.div`
+  color: ${({ theme }) => theme.headerText};
+  font-size: 30px;
+  font-family: ${({ theme }) => theme.secondaryFonts};
+  font-weight: 600;
+  font-size: 30px;
+  line-height: 36px;
+  display: flex;
+  align-items: center;
+  margin-left: 5%;
+  padding-top: 2%;
+`
+
+const SettingSubheading = styled.div`
+  color: ${({ theme }) => theme.text};
+  font-family: ${({ theme }) => theme.secondaryFonts};
+  margin-left: 5%;
+  font-weight: bold;
+  font-size: 20px;
+  line-height: 24px;
+  padding-top: 1%;
+  padding-bottom: 0.25%;
+`
+
+const SettingCaption = styled.div`
+  color: ${({ theme }) => theme.text};
+  font-family: ${({ theme }) => theme.secondaryFonts};
+  margin-left: 5%;
+  font-weight: 350;
+  font-size: 1rem;
+  line-height: 24px;
+  padding-top: 1%;
+  padding-bottom: 0.5%;
+`
+
+const SettingsText = styled.div`
+  color: ${({ theme }) => theme.text};
+  font-family: ${({ theme }) => theme.secondaryFonts};
+  margin-left: 5%;
+  font-size: 0.75rem;
+  line-height: 24px;
+  padding-top: 1%;
 `
 
 const IconContainer = styled.div`
@@ -66,39 +62,55 @@ const IconContainer = styled.div`
   gap: 2.5%;
 `
 
+const ToggleContainer = styled.div`
+  display: flex;
+  gap: 2%;
+`
+
+const ToggleStyle = styled.div`
+  padding-top: 1%;
+`
+
 function Settings() {
   const handle = useFullScreenHandle()
   return (
-    <SettingsSection>
+    <>
       <NavBar handle={handle} isFullScreen={true} atHome={false}></NavBar>
-      <h1>Clock Settings</h1>
-      <h3 id="news_ticker"> News Ticker</h3>
-      <p> Turn off/on the bottom news on your clock</p>
-      <h3 id="language">Configure Language</h3>
-      <h4> Language </h4>
+      <SettingsHeading>Clock Settings</SettingsHeading>
+
+      <ToggleContainer>
+        <SettingSubheading id="news_ticker"> News Ticker</SettingSubheading>
+        <ToggleStyle>
+          <Toggle />
+        </ToggleStyle>
+      </ToggleContainer>
+
+      <SettingsText> Turn off/on the bottom news on your clock</SettingsText>
+      <SettingSubheading id="language">Configure Language</SettingSubheading>
+      <SettingCaption> Language </SettingCaption>
       <StyledSelect options={options} />
-      <h3 id="share">Share your custom clock</h3>
-      <h4> Shareable Link </h4>
+      <SettingSubheading id="share">Share your custom clock</SettingSubheading>
+      <SettingCaption> Shareable Link </SettingCaption>
       <CopyButton
         type="link"
         placeholder="https://clock.climateclock.world/oGpVDQKb95lh"
       />
-      <h4>Embed</h4>
-      <p>
+      <SettingCaption>Embed</SettingCaption>
+      <SettingsText>
         To add the Climate Clock widget to your site, add the following HTML:
-      </p>
+      </SettingsText>
       <CopyButton
         type="embed"
         placeholder='<script src="https://climateclock.world/widget-v2.js" async></script><climate-clock /><sc '
       />
-      <h4>Social Media</h4>
+      <SettingCaption>Social Media</SettingCaption>
       <IconContainer>
         <img src={facebook} alt="facebook" />
         <img src={instagram} alt="instagram" />
         <img src={twitter} alt="twitter" />
         <img src={whatsapp} alt="whatsapp" />
       </IconContainer>
-    </SettingsSection>
+    </>
   )
 }
 
