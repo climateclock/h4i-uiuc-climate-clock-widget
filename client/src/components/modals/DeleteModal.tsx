58d8d019f1c16ff0f5bb172c4cdb6171e6b2d921
daniel-moon32 <dm32@illinois.edu>
2022-05-01 15:50:07 -0500

    format

--- client/src/components/modals/DeleteModal.tsx
+++ client/src/components/modals/DeleteModal.tsx
@@ -1,82 +1,78 @@
-import { useState } from 'react';
-import '@reach/dialog/styles.css';
-import { DialogContent, DialogOverlay } from '@reach/dialog';
+import { useState } from 'react'
+import '@reach/dialog/styles.css'
+import { DialogContent, DialogOverlay } from '@reach/dialog'
 import { Close } from '@styled-icons/evaicons-solid'
-import VisuallyHidden from "@reach/visually-hidden";
+import VisuallyHidden from '@reach/visually-hidden'
 import styled from 'styled-components'
-import StyledButton from '../buttons/button';
-
+import StyledButton from '../buttons/button'
 
 const CloseButton = styled(Close)`
   color: #575757;
   height: 36px;
   width: 36px;
   cursor: pointer;
-`;
-
+`
 
 const SubmitButton = styled(StyledButton)`
-    float: right;
-    font-size: 12px;
-`;
+  float: right;
+  font-size: 12px;
+`
 
 const StyledDialogContainer = styled.div`
-    .close-button {
-        float: right;
-    }
-    .element {
-        text-align: center;
-        padding-top: 60px;
-    }
-    .description {
-        text-align: center;
-        padding-top: 5px;
-        padding-bottom: 60px;
-        font-size: 16px;
-    }
-    .cancel {
-        float: right;
-        color: ${({ theme }) => theme.buttonBackground};
-        margin: 12px;
-        font-size: 12px;
-        background: none;
-        border-color: transparent;
-        cursor: pointer;
-    }
-
-`;
+  .close-button {
+    float: right;
+  }
+  .element {
+    text-align: center;
+    padding-top: 60px;
+  }
+  .description {
+    text-align: center;
+    padding-top: 5px;
+    padding-bottom: 60px;
+    font-size: 16px;
+  }
+  .cancel {
+    float: right;
+    color: ${({ theme }) => theme.buttonBackground};
+    margin: 12px;
+    font-size: 12px;
+    background: none;
+    border-color: transparent;
+    cursor: pointer;
+  }
+`
 
 const StyledHeader = styled.h1`
-    font-family: ${({ theme }) => theme.secondaryFonts};
-    font-size: 24px;
-    color: black;
-    font-weight: 500;
-`;
+  font-family: ${({ theme }) => theme.secondaryFonts};
+  font-size: 24px;
+  color: black;
+  font-weight: 500;
+`
 
 const StyledDescription = styled.p`
-    font-family: ${({ theme }) => theme.secondaryFonts};
-    font-size: 14px;
-`;
+  font-family: ${({ theme }) => theme.secondaryFonts};
+  font-size: 14px;
+`
 
 // const DialogStyle = styled.div`
 
 // `;
 
-
 function DeleteModal() {
-    const [showDialog, setShowDialog] = useState(false);
-    const open = () => setShowDialog(true);
-    const close = () => setShowDialog(false);
-    return(
+  const [showDialog, setShowDialog] = useState(false)
+  const open = () => setShowDialog(true)
+  const close = () => setShowDialog(false)
+  return (
     <div>
       <button onClick={open}>Show Dialog</button>
       <DialogOverlay isOpen={showDialog} onDismiss={close}>
-      <DialogContent
+        <DialogContent
           style={{
-            border: "solid 1px hsla(0, 0%, 0%, 0.5)",
-            borderRadius: "10px",
-            width: "325px",
-            height: "255px",
+            border: 'solid 1px hsla(0, 0%, 0%, 0.5)',
+            borderRadius: '10px',
+            width: '325px',
+            height: '255px',
             position: 'absolute',
             left: '50%',
             top: '50%',
@@ -90,20 +86,22 @@ function DeleteModal() {
             //percentage
           }}
         >
-        <StyledDialogContainer>
-        <CloseButton className="close-button" onClick={close}>
-           <VisuallyHidden>Close</VisuallyHidden>
+          <StyledDialogContainer>
+            <CloseButton className="close-button" onClick={close}>
+              <VisuallyHidden>Close</VisuallyHidden>
               <span aria-hidden>X</span>
-        </CloseButton>
-          <StyledHeader className="element">Delete Lifeline</StyledHeader>
-          <StyledDescription className="description">Do you want to delete this lifeline?</StyledDescription>
-          <SubmitButton buttonLabel={ "Delete" }></SubmitButton>
-          <button className="cancel">Cancel</button>
+            </CloseButton>
+            <StyledHeader className="element">Delete Lifeline</StyledHeader>
+            <StyledDescription className="description">
+              Do you want to delete this lifeline?
+            </StyledDescription>
+            <SubmitButton buttonLabel={'Delete'}></SubmitButton>
+            <button className="cancel">Cancel</button>
           </StyledDialogContainer>
         </DialogContent>
       </DialogOverlay>
     </div>
-    )
+  )
 }
 
-export default DeleteModal;
+export default DeleteModal
