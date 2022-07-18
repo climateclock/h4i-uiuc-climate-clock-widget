dacdbc6d858787e2da0b57f56da556e35bc7ed03
Daniel Moon <36148173+daniel-moon32@users.noreply.github.com>
2022-07-17 23:36:33 -0700

    Lifeline modals (#103)
    * Nav bar styling fixes (#96)
    
    * Align text, adjust sizing, and edit transition
    
    * Add nav bar to lifelines page
    
    * Rotate enter full screen icon
    
    * Move full screen hover effect to full screen component CSS
    
    * Reorder menu and full-screen icons and fix padding
    
    * Fix nav bar padding for both CC logo and icons
    
    * Run yarn format
    
    * Fix mobile nav bar transition and icon padding
    
    * Work on resizing clock container for fixed height nav bar
    
    * Make deadline container height responsive to number of lifelines
    
    * Run yarn format
    
    * remove required check
    
    * fix unit not showing bug
    
    Co-authored-by: Archna Sobti <68453619+Archna-1@users.noreply.github.com>

--- client/src/components/buttons/ExitFullscreen.tsx
+++ client/src/components/buttons/ExitFullscreen.tsx
@@ -2,16 +2,16 @@ import { FullscreenExit } from '@styled-icons/open-iconic'
 import { FullScreenHandle } from 'react-full-screen'
 import styled from 'styled-components'
 
-const ExitButton = styled.div`
-  color: white;
+const StyledFullscreenExit = styled(FullscreenExit)`
+  color: ${({ theme }) => theme.navBarText};
+
+  &:hover {
+    color: ${({ theme }) => theme.blue};
+  }
 `
 
 function ExitFullScreen({ handle }: { handle: FullScreenHandle }) {
-  return (
-    <ExitButton>
-      <FullscreenExit onClick={handle.exit} size="20px" />
-    </ExitButton>
-  )
+  return <StyledFullscreenExit onClick={handle.exit} size="20px" />
 }
 
 export default ExitFullScreen
